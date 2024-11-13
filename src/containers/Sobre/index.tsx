76320f444a3bee94311023a35f46917e76069349
Rafael Tamy <rafaeltamydb@gmail.com>
2024-11-13 16:59:33 -0300

    construindo a estrutura

---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,15 @@
+import Titulo from '../../components/Titulo'
+
+const Sobre = () => (
+  <section>
+    <Titulo>Sobre</Titulo>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium
+      consequuntur temporibus vel repudiandae, quod soluta ut vero iusto animi
+      non aliquid exercitationem et cumque tempore voluptatum provident nesciunt
+      maiores eveniet?
+    </p>
+  </section>
+)
+
+export default Sobre
